d87ac2456b2a391bea10ac7c3c868509bd832a63
Owen Daly-Furlong <owend@calastone.local>
2017-01-09 14:44:26 +0000

    Changed logfile location, move old logs

---
+++ MicroChallenge5/Logger.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace MicroChallenge5
+{
+    public class Logger
+    {
+        private string _logFileName;
+
+        public Logger(string fileName)
+        {
+            _logFileName = $"Logs\\{fileName}";
+        }
+
+        public bool Log(string message, Levels Level)
+        {
+            DateTime currentDateTime = DateTime.Now;
+            try
+            {
+                using (TextWriter logFile = File.AppendText(_logFileName))
+                {
+                    logFile.Write($"{currentDateTime}  {Level.ToString()}  {message}\n");
+                    logFile.Close();
+                    return true;
+                }
+            }
+            catch (SystemException) { return false; }
+        }
+
+        public bool MoveOldLog()
+        {
+            DateTime currentDateTime = DateTime.Now;
+            if (File.Exists(_logFileName))
+            {
+                try
+                {
+                    File.Move(_logFileName, $"{_logFileName}{currentDateTime}");
+                    return true;
+                }
+                catch (IOException ex)
+                {
+                    throw ex;
+                }
+            }
+            else { return true; }
+        }
+    }
+}
